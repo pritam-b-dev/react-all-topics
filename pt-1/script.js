393a53dfea31bb743ef459e5249eecd391352393
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-09-22 22:54:09 +0600

    dom manupulation vanilla js

--- pt-1/script.js
+++ pt-1/script.js
@@ -1,7 +1,12 @@
 const productPrice = 5000;
 const button = document.getElementById("button");
-let totalPrice = 0;
+const price = document.getElementById("price");
+const total = document.getElementById("total");
 
+let totalPrice = 0;
+price.innerText = `TK ${productPrice}`;
+total.innerText = `0`;
 button.addEventListener("click", () => {
   totalPrice += productPrice;
+  total.innerText = `TK ${totalPrice}`;
 });
